927f74a10a9fc58633f939422b818d36a9594ef6
JavaArdiandi <javaardiandi28@gmail.com>
2023-03-21 19:49:47 +0700

    cvjava

---
+++ resources/views/application/about.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('title', 'about')
+
+@section('about')
+<section id="about" class="about">
+    <div class="container" data-aos="fade-up">
+
+      <div class="section-title">
+        <h2>About</h2>
+        <p> Hello , My Name is Java Ardiandi , i'm student from Batam International University , i am majoring in information system</p>
+        <blockquote>"You Determine Your Own Future"  </blockquote>
+      </div>
+       </div>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <img src="assets/img/JavaArdiandi.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content">
+          <h3>Student &amp; Gamers.</h3>
+          <p class="fst-italic">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua.
+          </p>
+          <div class="row">
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>7 July 2003</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+628967792135</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Batam, Indonesia</span></li>
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>20</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>ML Mythic Glory</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>Javacarrysiskom@gmail.com</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Unavailable</span></li>
+              </ul>
+            </div>
+          </div>
+            </p>Hallo, mein Name ist Java Ardiandi, mein Hobby ist Videobearbeitung und Spiele und ich bin Student. Ich studiere und arbeite, um mir ein Taschengeld zu verdienen. Vielen Dank für Ihre Aufmerksamkeit.
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End About Section -->
+@endsection
---
+++ resources/views/application/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'home')
+
+@section('home')
+<section id="hero" class="d-flex flex-column justify-content-center">
+    <div class="container" data-aos="zoom-in" data-aos-delay="100">
+      <h1>JavaArdiandi</h1>
+      <p>I'm <span class="typed" data-typed-items="Student, Designer, Gamers, Animator"></span></p>
+      <div class="social-links">
+        <a href="https://facebook.com/Minzyy" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://instagram.com/java_ardiandii" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="https://github.com/JavaArdiandi" class="github"><i class="bx bxl-github"></i></a>
+      </div>
+    </div>
+  </section>
+@endsection()
